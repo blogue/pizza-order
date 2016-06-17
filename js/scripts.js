e270579fd65601687746179a8d57033ba198c843
Benjamin Logue <benjamin.logue73@gmail.com>
2016-06-17 10:42:01 -0700

    it will add option for user to order multiple pizzas, and a function to calculate total.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,9 @@
+function Order(name) {
+  this.name = name;
+  this.pizzasAdded = [];
+  this.total = 0;
+}
+
 function Pizza(size){
   this.size = size;
   this.toppings = [];
@@ -17,27 +23,43 @@ Pizza.prototype.calculateCost = function(){
   }
 }
 
+Order.prototype.calculateTotal = function(){
+  for (i=0; i < this.pizzasAdded.length; i++)
+  this.total += this.pizzasAdded[i].price;
+}
+
 
 $(document).ready(function(){
+  var newPizzaOrder;
 
-  $("#add-pizza").submit(function(event){
+  $("#add-pizza").click(function(){
+    var customer = $("#user-name").val();
+    return newPizzaOrder = new Order(customer);
+
+  });
+
+  $("#show-total").click(function(){
+     console.log(newPizzaOrder.calculateTotal());
+  });
+
+  $("#place-order").submit(function(event){
     event.preventDefault();
 
     var pizzaSize = $("input[name='size']:checked").val();
     var newPizza = new Pizza(pizzaSize);
-    // console.log($('input[name="topping"]:checked').serialize());
-
     $('input[name="topping"]:checked').each(function() {
        newPizza.toppings.push(this.value);
     });
-
     newPizza.calculateCost();
+    newPizzaOrder.pizzasAdded.push(newPizza);
 
+    console.log(newPizzaOrder);
     $('#order-total').text(newPizza.price);
     $('#pizza-size').text(newPizza.size);
     newPizza.toppings.forEach(function(topping){
-      $("#your-toppings").append("<li>" + topping + "</li>")
+    $("#your-toppings").append("<li>" + topping + "</li>")
     });
+
 
   });
 });
