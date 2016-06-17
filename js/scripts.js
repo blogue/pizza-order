d48ad58fbdf3542adbddfa96ea4e6c497854587a
Benjamin Logue <benjamin.logue73@gmail.com>
2016-06-17 15:31:55 -0700

    it will show customers address on order submit.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,7 @@
 function Order(name) {
   this.name = name;
   this.pizzasAdded = [];
+  this.address = [];
   this.total = 0;
 }
 
@@ -12,14 +13,14 @@ function Pizza(size){
 
 Pizza.prototype.calculateCost = function(){
   if (this.size === "small") {
-    this.price += 5;
+    this.price += 5.99;
   } else if (this.size === "medium") {
-    this.price += 8;
+    this.price += 8.99;
   } else  {
-    this.price += 10;
+    this.price += 10.99;
   }
   for (i=0; i < this.toppings.length; i++) {
-    this.price += 1;
+    this.price += 1.25;
   }
 }
 
@@ -30,21 +31,30 @@ Order.prototype.calculateTotal = function(){
 
 $(document).ready(function(){
   var newPizzaOrder;
-
-  $("#enter-name").click(function(){
+  $("#delivery").click(function(){
     var customer = $("#user-name").val();
+    var street = $("#street").val();
+    var city = $("#city").val();
+    var state = $("#state").val();
+
+    console.log(street);
     newPizzaOrder = new Order(customer);
+    newPizzaOrder.address.push(street);
+    newPizzaOrder.address.push(city);
+    newPizzaOrder.address.push(state);
+
     $("#user-information").hide();
-    $("#enter-name").hide();
+    $("#delivery").hide();
     $("#pizza-details").show();
     $(".menu-buttons").show();
   });
 
   $("#show-total").click(function(){
      console.log(newPizzaOrder.calculateTotal());
-     $("#output").prepend("<h3>Thanks for your purchase, "
+     $("#output").prepend("<h3>Thanks for your order, "
                   + newPizzaOrder.name + "!<br>"
                   + "Your total is: $" + newPizzaOrder.total + "</h3>");
+     $("#output").append("<h3>Delivery address: " + newPizzaOrder.address + "</h3>");
         $("#pizza-details").hide();
         $("button").hide();
   });
@@ -60,12 +70,10 @@ $(document).ready(function(){
     newPizza.calculateCost();
     newPizzaOrder.pizzasAdded.push(newPizza);
 
-    console.log(newPizzaOrder);
-
     $("#output").show();
     $("#output").prepend("<h3>Added to order: " + newPizza.size + " pizza.<br>"
             + "Toppings: " + newPizza.toppings + "<br>"
             + "Item price: $" + newPizza.price + "</h3>");
-
+    $("#place-order")[0].reset();
   });
 });
